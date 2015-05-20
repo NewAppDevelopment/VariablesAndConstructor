cdc4a0c185da4b8dbb0032e9a5a028d5d5d73fa8
Karthik S <CompIndia@CINET2.local>
2015-05-20 09:23:02 +0530

    Initial Commit

---
+++ VariablesAndConstructor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VariablesAndConstructor
+//
+//  Created by Karthik S on 20/05/15.
+//  Copyright (c) 2015 CompIndia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VariablesAndConstructor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VariablesAndConstructor
+//
+//  Created by Karthik S on 20/05/15.
+//  Copyright (c) 2015 CompIndia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
